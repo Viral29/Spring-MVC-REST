a742f83f2538f487c5dc3b211b1c8e69002c9638
Viral29 <viru2994@gmail.com>
2018-06-26 02:35:59 -0400

    Adding bootstrap class

---
+++ src/main/java/com/example/SpringmvcrestApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringmvcrestApplication {
+
+    public static void main(String[] args) {
+
+        ApplicationContext context = SpringApplication.run(SpringmvcrestApplication.class, args);
+
+        System.out.println("Bootstrap   "+context.containsBeanDefinition("Bootstrap"));
+
+
+    }
+}
